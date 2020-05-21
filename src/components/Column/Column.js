4556d8b0e32ace94538fa5bab8c93cf716e32f2d
Rafal Szymanko <szymanko@gmail.com>
2020-05-21 09:00:00 +0200

    Add script to render new card by Redux store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-// import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 import Card from '../Card/Card';
 import { settings } from '../../data/dataStore';
@@ -14,6 +13,8 @@ class Column extends React.Component {
     title: PropTypes.string,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
+
   }
 
   static defaultProps = {
@@ -21,7 +22,7 @@ class Column extends React.Component {
   }
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     console.log(cards);
     return (
       <section className={styles.component}>
@@ -32,9 +33,9 @@ class Column extends React.Component {
         {cards.map(cardData => (
           <Card key={cardData.id} {...cardData} />
         ))}
-        {/* <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
       </section>
     );
   }
